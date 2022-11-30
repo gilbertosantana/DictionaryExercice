70b4654ab6477cbebe80f8832e8c72552b12d3d7
gilbertosantana <gilbertosantoss307@gmail.com>
2022-11-29 22:10:32 -0300

    Add project files.

---
+++ DictionaryExercice/Program.cs
@@ -0,0 +1,35 @@
+﻿Dictionary<string, int> candidates = new Dictionary<string, int>();
+
+Console.Write("Enter full path: ");
+
+string path = @Console.ReadLine()!;
+
+try
+{
+    using(StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] votingRecord = sr.ReadLine()!.Split(',');
+            string candidate = votingRecord[0];
+            int votes = int.Parse(votingRecord[1]);
+
+            if (!candidates.ContainsKey(candidate))
+            {
+                candidates.Add(candidate, votes);
+            }
+            else
+            {
+                candidates[candidate] += votes;
+            }
+        }
+    }
+    foreach(var item in candidates)
+    {
+        Console.WriteLine(item.Key + ": " + item.Value);
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine(e.Message);
+}
